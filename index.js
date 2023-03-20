39be26ebc040e5811b087657633aed7633592cc3
Nicolas Massaccesi Perez <nicocarp1349@gmail.com>
2023-03-20 16:23:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const enlaces  = document.querySelectorAll('.ul .a')
-const lightbox = document.querySelector('.lightbox')
-const grande   = document.querySelector('.grande')
-const cerrar   = document.querySelector('.cerrar')
-
-enlaces.forEach(( cadaEnlace , i )=>{
-    enlaces[i].addEventListener('click', ( e )=>{
-        e.preventDefault()
-        let ruta = cadaEnlace.querySelector('.img').src
-        console.log( ruta ) 
-
-        lightbox.classList.add('activo')
-        grande.setAttribute('src', ruta)
-
-    })
-})
-
-cerrar.addEventListener('click', ()=>{
-    lightbox.classList.remove('activo')
-})
-
 window.addEventListener("scroll", function() {
   const navbar = document.querySelector(".navbar");
   if (window.scrollY > 0) {
